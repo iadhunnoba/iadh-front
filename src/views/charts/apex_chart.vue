16735ba425c3f20cd01869e69b859a591f25ca4e
Juan Pablo Beloso <juanpbeloso@gmail.com>
2024-06-15 17:18:44 -0300

    Emqx configs fix

--- src/views/charts/apex_chart.vue
+++ src/views/charts/apex_chart.vue
@@ -589,6 +589,7 @@ export default {
       series1: [{ data: data1.slice() }],
       series2: [{ data: data2.slice() }],
       series3: [{ data: data2.slice() }],
+      randomValue: 0,
       chartOptions: {
         chart: {
           animations: {
@@ -635,6 +636,40 @@ export default {
       timerPaused: false,     // Variable para controlar el estado de pausa del cronómetro
       ramainingTime: 180,          // Tiempo restante en segundos (3 minutos = 180 segundos)
       timerInterval: null,     // Referencia al interval del cronómetro
+
+      // EMQX CONNECTION VARS
+      connection: {
+        protocol: "ws",
+        host: "broker.emqx.io",
+        // ws: 8083; wss: 8084
+        port: 8083,
+        endpoint: "/mqtt",
+        // for more options, please refer to https://github.com/mqttjs/MQTT.js#mqttclientstreambuilder-options
+        clean: true,
+        connectTimeout: 30 * 1000, // ms
+        reconnectPeriod: 4000, // ms
+        clientId: "emqx_vue_" + Math.random().toString(16).substring(2, 8),
+        // auth
+        username: "emqx_test",
+        password: "emqx_test",
+      },
+      subscription: {
+        topic: "topic/mqttx",
+        qos: 0,
+      },
+      publish: {
+        topic: "topic/browser",
+        qos: 0,
+        payload: '{ "msg": "Hello, I am browser." }',
+      },
+      receiveNews: "",
+      qosList: [0, 1, 2],
+      client: {
+        connected: false,
+      },
+      subscribeSuccess: false,
+      connecting: false,
+      retryTimes: 0,
     };
   },
 
@@ -643,19 +678,21 @@ export default {
     // Me conecto al broker MQTT a través de WebSockets
 
     // Esta conexión es para cuando corro el servidor en la misma compu que la app
-    let client = mqtt.connect('ws://localhost:8083/mqtt');
+    //let client = mqtt.connect('ws://localhost:8083/mqtt');
     //let client = mqtt.connect('ws://192.168.0');
 
-    client.subscribe('/iadh');
+    //client.subscribe('/iadh');
 
     // Me mantengo escuchando al broker
-    client.on('message', function (topic, message) {
-      console.log(message.toString());
-    });
+    // client.on('message', function (topic, message) {
+    //   console.log(message.toString());
+    // });
 
-    client.publish('/iadh', '¡Buenas!');
+    //client.publish('/iadh', '¡Buenas!');
 
-
+    this.initData();
+    this.createConnection();
+    this.doSubscribe();
 
     // Gràfico del frecuencia cardíaca
     /* Declaro las variables que necesito para volver dinamico el gràfico: 
@@ -840,6 +877,67 @@ export default {
   },
 
   methods: {
+
+    initData() {
+      this.client = {
+        connected: false,
+      };
+      this.retryTimes = 0;
+      this.connecting = false;
+      this.subscribeSuccess = false;
+    },
+
+    handleOnReConnect() {
+      this.retryTimes += 1;
+      if (this.retryTimes > 5) {
+        try {
+          this.client.end();
+          this.initData();
+          this.$message.error("Connection maxReconnectTimes limit, stop retry");
+        } catch (error) {
+          this.$message.error(error.toString());
+        }
+      }
+    },
+
+    createConnection() {
+      try {
+        this.connecting = true;
+        const { protocol, host, port, endpoint, ...options } = this.connection;
+        const connectUrl = `${protocol}://${host}:${port}${endpoint}`;
+        this.client = mqtt.connect(connectUrl, options);
+        if (this.client.on) {
+          this.client.on("connect", () => {
+            this.connecting = false;
+            console.log("Connection succeeded!");
+          });
+          this.client.on("reconnect", this.handleOnReConnect);
+          this.client.on("error", (error) => {
+            console.log("Connection failed", error);
+          });
+          this.client.on("message", (topic, message) => {
+            this.receiveNews = this.receiveNews.concat(message);
+            console.log(`Received message ${message} from topic ${topic}`);
+          });
+        }
+      } catch (error) {
+        this.connecting = false;
+        console.log("mqtt.connect error", error);
+      }
+    },
+
+    doSubscribe() {
+      const { topic, qos } = this.subscription
+      this.client.subscribe(topic, { qos }, (error, res) => {
+        if (error) {
+          console.log('Subscribe to topics error', error)
+          return
+        }
+        this.subscribeSuccess = true
+        console.log('Subscribe to topics res', res)
+      })
+    },
+
     intervals: function () {
       var me = this;
       window.setInterval(function () {
